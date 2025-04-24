2c11f74ba7f8bbb209eb6aa502bb506f3a93fcde
NandhakumarIT26 <168270064+NandhakumarIT26@users.noreply.github.com>
2025-04-24 12:58:45 +0530

    Update Matches.jsx

--- frontend/src/pages/Matches.jsx
+++ frontend/src/pages/Matches.jsx
@@ -21,7 +21,7 @@ const Matches = () => {
             <h1 className="text-2xl font-bold my-4 text-center">Matches</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {matches.map((match) => (
-                    <MatchCard key={match.id} match={match} />
+                    <MatchCard match={match} />//key={match.id} 
                 ))}
             </div>
         </div>
